0f59713d25c0e70d1568de4ef7882e0bc262067a
DhruviJP <dhruvi8033@gmail.com>
2022-09-12 18:59:31 +0530

    Second-Cmt

--- src/Components/Pagination/CustomPagiantion.jsx
+++ src/Components/Pagination/CustomPagiantion.jsx
@@ -3,8 +3,7 @@ import Pagination from "@mui/material/Pagination";
 
 const CustomPagination = ({ setPage, numOfPages = 10 }) => {
     const handlePageChange = (page) => {
-        setPage(page)
-            ;
+        setPage(page);
         window.scroll(0, 0);
     };
     return (
